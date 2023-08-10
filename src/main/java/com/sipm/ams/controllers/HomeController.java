ffdcc8184c5e0ed5d5bff4358004a0f2e609212d
Guesmi Achref <guesmi.achraf3@gmail.com>
2023-08-10 22:50:15 +0200

    passage de data vers vue

--- src/main/java/com/sipm/ams/controllers/HomeController.java
+++ src/main/java/com/sipm/ams/controllers/HomeController.java
@@ -1,14 +1,18 @@
 package com.sipm.ams.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/info")// ce que je tape dans l`url
-	public String info() {
+	public String info(Model model) {
+		String formation= "Fullstack";
 		System.out.println("Méthode infoo");
+	   model.addAttribute("workshop", formation);
 		return "home/info";
 	}
 	
