067baa3d307d28eeab0686762eb41fb8a3554160
Guesmi Achref <guesmi.achraf3@gmail.com>
2023-08-10 20:48:57 +0200

    first commit

---
+++ src/main/java/com/sipm/ams/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.sipm.ams.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/info")// ce que je tape dans l`url
+	public String info() {
+		System.out.println("Méthode infoo");
+		return "home/info";
+	}
+	
+	@RequestMapping("/affichage")// le nom de la vue sous template
+	public String affichage() {
+		return "home/affichage";
+	
+}
+}
